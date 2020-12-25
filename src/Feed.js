cb78b08beabfc6f2ad6607c71ac518cc111103b1
amooryjubran <omarsalehjubran@gmail.com>
2020-12-24 23:41:49 -0500

    Post part 1

---
+++ src/Feed.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed_inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" placeholder="Start A Post Here" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7fc15e"
+          />
+        </div>
+      </div>
+      {/*  */}
+      <Post />
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/Post.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Post.css";
+function Post({ name, descreption, messeage, photoUrl }) {
+  return <div className="post"></div>;
+}
+
+export default Post;
