e7b7b94009eced53f975ae34aa537b4772367fe8
amooryjubran <omarsalehjubran@gmail.com>
2021-01-01 23:11:30 -0500

    firebase fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqVXwncheL8EmOszDAoFGw42Cm2cRfBok",
-  authDomain: "linkedin-f902b.firebaseapp.com",
-  projectId: "linkedin-f902b",
-  storageBucket: "linkedin-f902b.appspot.com",
-  messagingSenderId: "1246779129",
-  appId: "1:1246779129:web:562aba6402dcba0954f428",
-  measurementId: "G-L1NGZ1JQEL",
+  apiKey: "AIzaSyD-b2q01fdn5DDX48GLrvnHIoMlRll5Ips",
+  authDomain: "linkedin-c0844.firebaseapp.com",
+  projectId: "linkedin-c0844",
+  storageBucket: "linkedin-c0844.appspot.com",
+  messagingSenderId: "476025352970",
+  appId: "1:476025352970:web:99bbb303d7c8b5cef900f3",
+  measurementId: "G-NXCNPTWPMK",
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
